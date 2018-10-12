cdf03fb33f64f908c0fe78a4f43e0644accbd996
unknown <lixuan.lim.2016@sis.smu.edu.sg>
2018-10-12 16:19:22 +0800

    push!

--- halogen/src/pages/insights.js
+++ halogen/src/pages/insights.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import image1 from "./radar.PNG";
+import image2 from "./Screenshot (66).png";
+import image3 from "./Screenshot (67).png";
+import image4 from "./Screenshot (68).png";
+import image5 from "./Screenshot (69).png";
+import image6 from "./Screenshot (70).png";
 
 class insights extends Component {
     render() {
         return(
         //Form here
         <div>
-            <h2> Insights </h2>
+            <h2> Insights - RADAR CHART </h2>
+            <img src={image1}/>
+           <div/>
+           <div/>
+           <h2> Anomaly - BOXPLOT</h2>
+           <img src={image5}/>
+           <h2> Anolomy - FORECASTING</h2>
+            <img src={image3}/>
+            <img src={image4}/>
+            
+            <img src={image6}/>
+            
         </div>
         );
     }
